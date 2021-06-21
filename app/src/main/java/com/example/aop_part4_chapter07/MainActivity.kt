e0d796b95479abd575ec99db56d36ee7da369e97
root <root@neodreams.co.kr>
2021-06-21 10:22:37 +0900

    이미지 저장 기능 추가중

--- app/src/main/java/com/example/aop_part4_chapter07/MainActivity.kt
+++ app/src/main/java/com/example/aop_part4_chapter07/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.aop_part4_chapter07
 
 import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.aop_part4_chapter07.Retrofit.RetrofitClient
 import com.example.aop_part4_chapter07.SearchResult.SearchResponseItem
 import com.example.aop_part4_chapter07.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -37,7 +43,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 	private fun initRecyclerView() = with(binding) {
 		if (unsplashAdapter == null) {
 			unsplashAdapter = UnsplashAdapter(onClicked = { photo ->
-				downloadImage(photo)
+				showDialogForSavePhoto(photo)
 			})
 		}
 
@@ -84,9 +90,93 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 		}
 	}
 
-	private fun downloadImage(photo : SearchResponseItem) {
-		//TODO: 선택 사진 다운로드
-		// 다운로드 완료 -> 배경화면 지정 액션 설정
+	private fun showDialogForSavePhoto(photo : SearchResponseItem) {
+		//TODO: 선택한 사진에 대해 저장 다이얼로그 표출
+	 // 저장 버튼 클릭 -> 다운로드 이미지
+
+		AlertDialog.Builder(this)
+			.setMessage("이 사진을 저장하시겠습니까?")
+			.setPositiveButton("저장"){ dialog, _ ->
+				//TODO: 이미지 다운로드
+				downloadPhoto(photo.urls?.full)
+				dialog.dismiss()
+			}
+			.setNegativeButton("취소"){ dialog, _ ->
+				dialog.dismiss()
+			}
+			.create()
+			.show()
+	}
+
+	private fun downloadPhoto(photoUrl : String?) {
+		photoUrl ?: return
+
+		Glide.with(this)
+			.asBitmap()
+			.load(photoUrl)
+			.diskCacheStrategy(DiskCacheStrategy.NONE)
+			.into(
+				object : CustomTarget<Bitmap> (SIZE_ORIGINAL, SIZE_ORIGINAL) {
+					override fun onResourceReady(
+						resource: Bitmap,
+						transition: Transition<in Bitmap>?
+					) {
+						saveBitmap(resource)
+					}
+
+					override fun onLoadCleared(placeholder: Drawable?) = Unit
+
+					override fun onLoadStarted(placeholder: Drawable?) {
+						super.onLoadStarted(placeholder)
+						Snackbar.make(
+							binding.root,
+							"다운로드 중..",
+							Snackbar.LENGTH_INDEFINITE
+						).show()
+					}
+
+					override fun onLoadFailed(errorDrawable: Drawable?) {
+						super.onLoadFailed(errorDrawable)
+						Snackbar.make(
+							binding.root,
+							"다운로드 실패",
+							Snackbar.LENGTH_INDEFINITE
+						).show()
+					}
+				}
+			)
+
+	}
+
+	private fun saveBitmap(resource: Bitmap) {
+		val fileName = "${System.currentTimeMillis()}.jpg"
+		val resolver = applicationContext.contentResolver
+		val imageCollectionUri = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+			MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+		} else {
+			MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+		}
+		val imageDetails = ContentValues().apply {
+			put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+			put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+
+			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+				put(MediaStore.Images.Media.IS_PENDING, 1)
+			}
+		}
+
+		val imageUri = resolver.insert(imageCollectionUri, imageDetails)
+		imageUri ?: return
+
+		resolver.openOutputStream(imageUri).use { outputStream ->
+			resource.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+		}
+
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+			imageDetails.clear()
+			imageDetails.put(MediaStore.Images.Media.IS_PENDING, 0)
+			resolver.update(imageUri, imageDetails, null, null)
+		}
 	}
 
 	override val coroutineContext: CoroutineContext
